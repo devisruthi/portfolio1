541403b40a0c0cdb81014a7781ef1c4d9548e53d
Sruthi <devisruthi@gmail.com>
2020-11-14 09:41:08 +0400

    Add content to about section and icons

--- js/scripts.js
+++ js/scripts.js
@@ -40,3 +40,18 @@
         target: "#sideNav",
     });
 })(jQuery); // End of use strict
+
+$(document).ready(function() {
+    $("#toggle").click(function() {
+      var elem = $("#toggle").text();
+      if (elem == "Read More") {
+        //Stuff to do when btn is in the read more state
+        $("#toggle").text("Read Less");
+        $("#text").slideDown();
+      } else {
+        //Stuff to do when btn is in the read less state
+        $("#toggle").text("Read More");
+        $("#text").slideUp();
+      }
+    });
+  });
